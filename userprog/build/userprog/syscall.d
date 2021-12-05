5f3444ff07c0a748836b983076c68014aa57b7a7
Ashokkumar <Ashok>
2021-12-05 18:00:22 +0530

    syscalls implemented

---
+++ userprog/build/userprog/syscall.d
@@ -0,0 +1,9 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+ ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+ ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
+ ../../threads/interrupt.h ../../threads/thread.h ../../lib/kernel/list.h \
+ ../../threads/synch.h ../../userprog/process.h ../../threads/vaddr.h \
+ ../../threads/loader.h ../../userprog/pagedir.h ../../devices/shutdown.h \
+ ../../filesys/filesys.h ../../filesys/off_t.h ../../threads/malloc.h \
+ ../../filesys/file.h ../../devices/input.h
